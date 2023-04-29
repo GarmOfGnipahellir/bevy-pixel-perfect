8e23cf2bbb2bf8cb8ca9f4dbbed5f78404b6a769
Garm <melsom.henrik@gmail.com>
2023-04-29 02:24:43 +0200

    🚧 Basic test scene

--- examples/basic.rs
+++ examples/basic.rs
@@ -1,1 +1,25 @@
-fn main() {}
+use bevy::prelude::*;
+
+fn main() {
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_startup_system(setup)
+        .run();
+}
+
+fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
+    commands.spawn(Camera3dBundle {
+        transform: Transform::from_xyz(0.0, 2.5, 2.5).looking_at(Vec3::ZERO, Vec3::Y),
+        ..Default::default()
+    });
+
+    commands.spawn(DirectionalLightBundle {
+        transform: Transform::from_xyz(1.0, 1.0, -1.0).looking_at(Vec3::ZERO, Vec3::Y),
+        ..Default::default()
+    });
+
+    commands.spawn(SceneBundle {
+        scene: asset_server.load("DamagedHelmet.glb#Scene0"),
+        ..Default::default()
+    });
+}
